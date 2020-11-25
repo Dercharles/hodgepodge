ce8db393bf5573609a040a7b0c0d8c3db342fa11
peng.yang <peng.yang@baozun.com>
2020-11-25 14:43:03 +0800

    init

---
+++ src/main/java/com/example/hodgepodge/HodgepodgeApplication.java
@@ -0,0 +1,16 @@
+package com.example.hodgepodge;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class HodgepodgeApplication {
+
+    public static void main(String[] args) {
+//        new SpringApplicationBuilder().bannerMode(Banner.Mode.OFF).run();
+        SpringApplication.run(HodgepodgeApplication.class, args);
+    }
+
+}
